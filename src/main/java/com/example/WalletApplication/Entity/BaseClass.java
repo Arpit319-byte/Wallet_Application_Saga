f7120a5fc409759203659e1d60a5854df4b4137f
Arpit Chauhan <arpitchauhan@192.168.1.2>
2025-11-06 18:13:44 +0530

    Added the entities and proprties in application file

---
+++ src/main/java/com/example/WalletApplication/Entity/BaseClass.java
@@ -0,0 +1,42 @@
+package com.example.WalletApplication.Entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    protected LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    protected LocalDateTime updatedAt;
+    
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+    
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/example/WalletApplication/Entity/User.java
@@ -0,0 +1,5 @@
+package com.example.WalletApplication.Entity;
+
+public class User {
+    
+}
