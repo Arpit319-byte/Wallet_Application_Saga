68a99597e0085bd4dfe28c1e3f08353190a904e9
Arpit Chauhan <arpitchauhan@192.168.1.2>
2025-11-06 16:35:50 +0530

    First Commit

---
+++ gradle/WalletApplicationTwoPhase/src/test/java/com/example/WalletApplicationTwoPhase/WalletApplicationTwoPhaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WalletApplicationTwoPhase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTwoPhaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
